2bc9ba74985507fc5617e52b28a1b7cd6cdcead3
Priyank <priyank1985gupta@rediffmail.com>
2014-09-23 16:19:19 +0530

    clustering fix

---
+++ config.js
@@ -0,0 +1,10 @@
+var configuration = {};
+
+var redis = {
+	port : 6379,
+	host : 54.68.216.148
+};
+
+
+configuration.redis = redis;
+module.exports = configuration;
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require('express')
 var app = express();
+var config = require('./config')
 var socketIo = require('socket.io');
+var redis = require("redis");
 
+var client = redis.createClient(config.redis.port, config.redis.host,{});
+var clientForChat = redis.createClient(config.redis.port, config.redis.host,{});
+var clientForJoin = redis.createClient(config.redis.port, config.redis.host,{});
 
 /*
 Error handling if we are unable to connect to redis
 */
+client.on("error", function(err){
+  console.log("Error %s", error);
+});
+
 
 var server = app.listen(3000, function(){
-	 console.log('listening on port :3000');
+  console.log('listening on port :3000');
 })
 
 var io = socketIo.listen(server);
@@ -27,18 +36,31 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
 
-  socket.on('join', function(name, room){
-  	 socket.join(room);
-  	 socket.username = name;
-  	 socket.room = room;
-  	 socket.emit('chat message', socket.username + " joined.");
-     socket.broadcast.to(socket.room).emit('chat message', socket.username + " joined.");
+  socket.on('join', function(info){
+    socket.join(info.room);
+    socket.username = info.username;
+    socket.room = info.room;
+    client.publish("join message",JSON.stringify({room: info.room ,message: socket.username + " joined."});
   });
 
-  socket.on('chat message', function(msg){
-  	socket.emit('chat message', socket.username + " : "+ msg);
-     socket.broadcast.to(socket.room).emit('chat message', socket.username + " : "+ msg);
+  socket.on('chat message', function(msgObj){
+    client.publish("chat message" , JSON.stringify({room: socket.room, message: socket.username + " : "+ msgObj.message}));
   });
 });
+
+clientForChat.on("message", function (channel, messageString) {
+  var messageObject = JSON.parse(messageString);
+  console.log("chat event catched : " + messageObject.room + " : "+messageObject.message);
+  io.to(messageObject.room).emit('chat message', messageObject.message);
+});
+
+clientForJoin.on("message", function (channel, messageString) {
+  var messageObject = JSON.parse(messageString);
+  console.log("join event catched : " + messageObject.room + " : "+messageObject.message);
+  io.to(messageObject.room).emit('chat message', messageObject.message);
+});
+
+clientForChat.subscribe("chat message");
+clientForJoin.subscribe("join message");
 
 app.use('/', express.static(__dirname + '/app'));
